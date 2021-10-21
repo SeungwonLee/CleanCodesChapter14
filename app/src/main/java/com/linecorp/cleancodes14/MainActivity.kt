fc90d0be45886aac73292909b016ae3661f61f9f
Seung won Lee (Ellen) <swsw.lee@linecorp.com>
2021-10-21 17:55:22 +0900

    added main logics

---
+++ app/src/main/java/com/linecorp/cleancodes14/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.linecorp.cleancodes14
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.linecorp.cleancodes14.refactored2.Args
+import com.linecorp.cleancodes14.refactored2.ArgsException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        try {
+            val schema = "l,p#,d*"
+            val args = arrayOfNulls<String>(4)
+            args[0] = "-p"
+            args[1] = "10"
+            args[2] = "-d"
+            args[3] = "Hi"
+            val arg = Args(schema, args)
+            val logging = arg.getBoolean('l')
+            val port = arg.getInt('p')
+            val directory = arg.getString('d')
+
+            Log.d("TAG", "onCreate: $logging $port $directory")
+            // executeApplication(logging, port, directory);
+        } catch (e: ArgsException) {
+            try {
+                System.out.printf("Argument error: %s\n", e.errorMessage())
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+        }
+    }
+}
